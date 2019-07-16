b90ef51eca1d8ccd334011636e9d2ab052045054
MsMACee <dimitraliveri@MsMACees-Air.lan>
2019-07-17 00:10:36 +0300

    Updates

---
+++ precache-manifest.674f33ee88a2c29442c89bc23d948c4d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff870c0a6243fa7f79bba25a75887e8d",
+    "url": "/prodtest/index.html"
+  },
+  {
+    "revision": "821428e5bd932489c6dd",
+    "url": "/prodtest/static/css/main.4f2980a1.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/prodtest/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "821428e5bd932489c6dd",
+    "url": "/prodtest/static/js/main.9898e9a7.chunk.js"
+  },
+  {
+    "revision": "ce2d8f0531e3da28fa48",
+    "url": "/prodtest/static/js/runtime~main.ce783392.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/prodtest/static/media/logo.5d5d9eef.svg"
+  }
+]);
